55239fd4a34d5017a68517d3f27db2ab8a00e4d9
morgancfarrow90 <63519344+morgancfarrow90@users.noreply.github.com>
2020-11-16 17:33:09 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+// Selectors
+
+document.querySelector('form').addEventListener('submit', handleSubmitForm);
+document.querySelector('ul').addEventListener('click', handleClickDeleteOrCheck)
+document.getElementById('clearAll').addEventListener('click', handleClearAll)
+
+// Event Handler Function
+
+function handleSubmitForm(e) {
+  e.preventDefault();
+  let input= document.querySelector('input');
+  if (input.value != "")
+    addTodo(input.value);
+  input.value = ''
+}
+
+function handleClickDeleteOrCheck(e) {
+  if (e.target.name == "checkButton")
+    checkTodo(e);
+
+  if (e.target.name == "deleteButton")
+    deleteTodo(e);
+}
+
+function handleClearAll(e) {
+  document.querySelector('ul').innerHTML= '';
+}
+
+// Helper Function
+function addTodo(todo){
+  let ul= document.querySelector('ul');
+  let li= document.createElement('li');
+
+  li.innerHTML =`
+    <span class= "todo-item">${todo}</span>
+    <button name="checkButton">Complete</button>
+    <button name="deleteButton">Delete</button>
+  `
+  li.classList.add('todo-list-item')
+  ul.appendChild(li)
+}
+
+function checkTodo(e){
+  let item= e.target.parentNode;
+  if (item.style.textDecoration == "line-through")
+    item.stye.textDecoration = 'none';
+  else
+    item.style.textDecoration= "line-through"
+}
+
+function deleteTodo(e){
+  let item= e.target.parentNode;
+
+  item.addEventListener('transitionend', function () {
+    item.remove();
+  })
+  item.classList.add('todo-list-item-fall');
+}
